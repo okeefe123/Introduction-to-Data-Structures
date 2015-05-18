7e338263e456ae6cf9a72930d8d381cdbfef4d47
Okeefe <okeefeniemann@gmail.com>
2015-05-18 12:27:59 -0700

    CMPS12B

---
+++ 3D-Array-(Calendar)/test.java
@@ -0,0 +1,9 @@
+class test {
+    public static void main(String[] args) {
+        int odd = 7;
+        int divisor = 2;
+        System.out.printf("%d\n", odd/divisor);
+        //prints out 3
+        //remainder gets thrown out
+    }
+}
